de5dc3d79044ca0328f61c9c59c1b4b2e094a355
Glitch (frosted-drum) <none>
2020-06-29 03:05:19 +0000

    🐧✌️ Checkpoint ./public/js/app.js:17335579/11717

--- public/js/app.js
+++ public/js/app.js
@@ -66,15 +66,15 @@ jQuery(function($) {
         .on("change", ".toggle", this.toggle.bind(this))
         .on("dblclick", "label", this.edit.bind(this))
         .on("keyup", ".edit", this.editKeyup.bind(this))
-        .on("focusout", ".edit", this.update.bind(this));
-      // .on("click", ".destroy", this.destroy.bind(this));
-      var todoList = document.getElementById("todo-list");
-      todoList.addEventListener("click", function(event) {
-        var elementClicked = event.target;
-        if (elementClicked.className === "destroy") {
-          App.destroy();
-        }
-      });
+        .on("focusout", ".edit", this.update.bind(this))
+        .on("click", ".destroy", this.destroy.bind(this));
+      // var todoList = document.getElementById("todo-list");
+      // todoList.addEventListener("click", function(event) {
+      //   var elementClicked = event.target;
+      //   if (elementClicked.className === "destroy") {
+      //     App.destroy();
+      //   }
+      // });
     },
     render: function() {
       var todos = this.getFilteredTodos();
