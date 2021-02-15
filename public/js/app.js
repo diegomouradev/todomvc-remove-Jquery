074b8ba7c2f84080d37d228b771ac2a9d81c0112
Diego Moura <59176835+diegomouradev@users.noreply.github.com>
2021-02-15 11:44:09 -0500

    updated for Gordon's Code Review

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-Handlebars.registerHelper("eq", function(a, b, options) {
+Handlebars.registerHelper("eq", function (a, b, options) {
   return a === b ? options.fn(this) : options.inverse(this);
 });
 
@@ -6,7 +6,7 @@ var ENTER_KEY = 13;
 var ESCAPE_KEY = 27;
 
 var util = {
-  uuid: function() {
+  uuid: function () {
     /*jshint bitwise:false */
     var i, random;
     var uuid = "";
@@ -23,148 +23,141 @@ var util = {
 
     return uuid;
   },
-  pluralize: function(count, word) {
+  pluralize: function (count, word) {
     return count === 1 ? word : word + "s";
   },
-  store: function(namespace, data) {
+  store: function (namespace, data) {
     if (arguments.length > 1) {
       return localStorage.setItem(namespace, JSON.stringify(data));
     } else {
       var store = localStorage.getItem(namespace);
       return (store && JSON.parse(store)) || [];
     }
-  }
+  },
 };
 
-
-var newTodo;
-const todoList = document.getElementById('todo-list');
-
 var App = {
-  init: function() {
+  init: function () {
     this.todos = util.store("todos-nojquery");
-    this.todoTemplate = Handlebars.compile(document.getElementById("todo-template").innerHTML);
-    this.footerTemplate = Handlebars.compile(document.getElementById("footer-template").innerHTML);
+    this.todoTemplate = Handlebars.compile(
+      document.getElementById("todo-template").innerHTML
+    );
+    this.footerTemplate = Handlebars.compile(
+      document.getElementById("footer-template").innerHTML
+    );
+
+    this.newTodo = document.getElementById("new-todo");
+    this.todoList = document.getElementById("todo-list");
+    this.toggleAllBtn = document.getElementById("toggle-all");
+
     this.bindEvents();
-    
+
     new Router({
-      "/:filter": function(filter) {
+      "/:filter": function (filter) {
         this.filter = filter;
         this.render();
-      }.bind(this)
+      }.bind(this),
     }).init("/all");
   },
-  bindEvents: function() {
-    
-    newTodo = document.getElementById('new-todo');
-    newTodo.addEventListener('keyup', this.create.bind(this));
+  bindEvents: function () {
+    this.newTodo.addEventListener("keyup", this.create.bind(this));
+    this.toggleAllBtn.addEventListener("change", this.toggleAll.bind(this));
 
-    // var declared outside of the object.
-    var toggleAll = document.getElementById('toggle-all');
-    toggleAll.addEventListener('change', this.toggleAll.bind(this));
-
-    var clearCompleted = document.getElementById('footer');
-    clearCompleted.addEventListener('click', function(){
+    var clearCompleted = document.getElementById("footer");
+    clearCompleted.addEventListener("click", function () {
       var elementClicked = event.target.id;
-      if (elementClicked === 'clear-completed')
-      App.destroyCompleted(event);
+      if (elementClicked === "clear-completed") App.destroyCompleted(event);
     });
 
-    //const todoList declared outside of the object for global scope.  
-    todoList.addEventListener('change', function() {
+    this.todoList.addEventListener("change", function () {
       var elementClicked = event.target;
-      if (elementClicked.className === 'toggle')
-        App.toggle(event);
+      if (elementClicked.className === "toggle") App.toggle(event);
     });
-    todoList.addEventListener('click', function() {
+    this.todoList.addEventListener("click", function () {
       var elementClicked = event.target;
       var numberOfClicks = event.detail;
-      if (elementClicked.className === 'destroy' && numberOfClicks === 1 )
+      if (elementClicked.className === "destroy" && numberOfClicks === 1)
         App.destroy(event);
     });
-    todoList.addEventListener('dblclick', function(){
+    this.todoList.addEventListener("dblclick", function () {
       var elementClicked = event.target;
       var numberOfClicks = event.detail;
-      if (elementClicked.tagName === 'LABEL' && numberOfClicks === 2 )
+      if (elementClicked.tagName === "LABEL" && numberOfClicks === 2)
         App.edit(event);
     });
 
-    todoList.addEventListener('keyup', this.editKeyup.bind(this));
+    this.todoList.addEventListener("keyup", this.editKeyup.bind(this));
 
-    todoList.addEventListener('focusout', function(){
+    this.todoList.addEventListener("focusout", function () {
       var elementClicked = event.target;
-      if (elementClicked.className === 'edit')
-      App.update(event);
+      if (elementClicked.className === "edit") App.update(event);
     });
   },
-  render: function() {
+  render: function () {
     var todos = this.getFilteredTodos();
-    var main  = document.getElementById('main');
-    var toggleAllButton = document.getElementById('toggle-all');
-
+    var main = document.getElementById("main");
     // todoTemplateHTML is assigned the handlebars template with the todos array passed in it.
     var todoTemplateHTML = this.todoTemplate(todos);
-    
-    todoList.innerHTML = todoTemplateHTML;
+    this.todoList.innerHTML = todoTemplateHTML;
 
     //display todos list if the count is more than 1.
     if (todos.length > 0) {
       main.style.display = "block";
     } else {
-      main.style.display = 'none';
-    };
+      main.style.display = "none";
+    }
 
     // active and unable state for the toggle all button.
     if (this.getActiveTodos().length === 0) {
-      toggleAllButton.checked = true;
+      this.toggleAllBtn.checked = true;
     } else {
-      toggleAllButton.checked = false;
-    };
+      this.toggleAllBtn.checked = false;
+    }
 
-    this.assembleTemplate();
+    // this.assembleTemplate();
 
     this.renderFooter();
-    newTodo.focus();
+    this.newTodo.focus();
     util.store("todos-nojquery", this.todos);
   },
-    renderFooter: function() {
+  renderFooter: function () {
     var todoCount = this.todos.length;
     var activeTodoCount = this.getActiveTodos().length;
     var footerTemplateHTML = this.footerTemplate({
       activeTodoCount: activeTodoCount,
       activeTodoWord: util.pluralize(activeTodoCount, "item"),
       completedTodos: todoCount - activeTodoCount,
-      filter: this.filter
+      filter: this.filter,
     });
 
-    var footer = document.getElementById('footer');
+    var footer = document.getElementById("footer");
     if (todoCount > 0) {
-      footer.style.display = 'block';
+      footer.style.display = "block";
       footer.innerHTML = footerTemplateHTML;
     } else {
-      footer.style.display = 'none';
+      footer.style.display = "none";
     }
   },
-  toggleAll: function(event) {
+  toggleAll: function (event) {
     var isChecked = event.target.checked;
 
-    this.todos.forEach(function(todo) {
+    this.todos.forEach(function (todo) {
       todo.completed = isChecked;
     });
 
     this.render();
   },
-  getActiveTodos: function() {
-    return this.todos.filter(function(todo) {
+  getActiveTodos: function () {
+    return this.todos.filter(function (todo) {
       return !todo.completed;
     });
   },
-  getCompletedTodos: function() {
-    return this.todos.filter(function(todo) {
+  getCompletedTodos: function () {
+    return this.todos.filter(function (todo) {
       return todo.completed;
     });
   },
-  getFilteredTodos: function() {
+  getFilteredTodos: function () {
     if (this.filter === "active") {
       return this.getActiveTodos();
     }
@@ -175,15 +168,15 @@ var App = {
 
     return this.todos;
   },
-  destroyCompleted: function() {
+  destroyCompleted: function () {
     this.todos = this.getActiveTodos();
     this.filter = "all";
     this.render();
   },
   // accepts an element from inside the `.item` div and
   // returns the corresponding index in the `todos` array
-  indexFromEl: function(elementClicked) {
-    var id = elementClicked.closest('li').dataset.id;
+  indexFromEl: function (elementClicked) {
+    var id = elementClicked.closest("li").dataset.id;
     var todos = this.todos;
     var i = todos.length;
 
@@ -193,7 +186,7 @@ var App = {
       }
     }
   },
-  create: function(event) {
+  create: function (event) {
     var input = event.target;
     var val = input.value;
 
@@ -204,25 +197,25 @@ var App = {
     this.todos.push({
       id: util.uuid(),
       title: val,
-      completed: false
+      completed: false,
     });
 
-    newTodo.value = "";
+    this.newTodo.value = "";
 
     this.render();
   },
-  toggle: function(event) {
+  toggle: function (event) {
     var i = this.indexFromEl(event.target);
     this.todos[i].completed = !this.todos[i].completed;
     this.render();
   },
-  edit: function(event) {
-    var todoLi = event.target.closest('li');
-    todoLi.classList.add('editing');
-    var input = todoLi.querySelector('.edit');
+  edit: function (event) {
+    var todoLi = event.target.closest("li");
+    todoLi.classList.add("editing");
+    var input = todoLi.querySelector(".edit");
     input.focus();
   },
-  editKeyup: function(event) {
+  editKeyup: function (event) {
     if (event.which === ENTER_KEY) {
       event.target.blur();
     }
@@ -233,11 +226,11 @@ var App = {
       setEventAttribute.blur();
     }
   },
-  update: function(event) {
+  update: function (event) {
     var el = event.target;
     var val = el.value.trim();
 
-    if (!val && el === 'input.edit') {
+    if (!val && el === "input.edit") {
       this.destroy(event);
       return;
     }
@@ -250,9 +243,9 @@ var App = {
 
     this.render();
   },
-  destroy: function(event) {
+  destroy: function (event) {
     this.todos.splice(this.indexFromEl(event.target), 1);
     this.render();
-  }
+  },
 };
 App.init();
